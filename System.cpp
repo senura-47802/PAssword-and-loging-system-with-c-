95a045eb9ea951427960ed0110aed77c9b85b4e1
Senura Perera <88277850+senura-47802@users.noreply.github.com>
2024-05-15 21:47:08 +0530

    Update System.cpp

--- System.cpp
+++ System.cpp
@@ -6,16 +6,19 @@ int main ()
 {
   int p;
   string u;
+  string e;
   cout << "What is your username:";
   cin >> u;
+  cout << "What is your Email:";
+  cin >> e;
   cout << "What is your Password:";
   cin >> p;
-  if (u == "Senura" && p == 2008)
+  if (u == "Senura" && p == 2008 && e == "senura20140116@gmail.com")
     {
-      cout << "Login sucessed!";
+      cout << "Login sucessed! Well done.";
     }
-  else;
+  else
   {
-    cout << "Login not sucessed!";
+    cout << "Login not sucessed! Try again.";
   }
 }
